272d725cf5c0006386fca089d7056db88f2cceae
Robert Graeber <bgraeber@gmail.com>
2013-05-20 05:54:13 -0700

    Tweaked UI Text, now converts -ipadhd images to a -hd and default version, removed save location prompt, now auto-saves in same folder

--- Unretiner/Classes/Utilities/NSURL+Unretina.m
+++ Unretiner/Classes/Utilities/NSURL+Unretina.m
@@ -28,74 +28,76 @@ static NSString* const kIpadHDString = @"-ipadhd";
         
     }
     //if ([self isRetinaImage]) {
-        // New path is the same file minus the @2x
-        NSString* newFilename = [[self lastPathComponent] stringByReplacingOccurrencesOfString:@"@2x" withString:@""];
-        newFilename = [newFilename stringByReplacingOccurrencesOfString:@"-hd" withString:@""];
-        newFilename = [newFilename stringByReplacingOccurrencesOfString:@"-ipadhd" withString:@"-ipad"];
-        NSString* newPath = [NSString stringWithFormat:@"%@%@", [folder relativeString], newFilename];
-        NSURL* newUrl = [NSURL URLWithString:newPath];
+    // New path is the same file minus the @2x
+    NSString* newFilename = [[self lastPathComponent] stringByReplacingOccurrencesOfString:@"@2x" withString:@""];
+    newFilename = [newFilename stringByReplacingOccurrencesOfString:@"-hd" withString:@""];
+    newFilename = [newFilename stringByReplacingOccurrencesOfString:@"-ipadhd" withString:@"-hd"];
+    NSString* newPath = [NSString stringWithFormat:@"%@%@", [folder relativeString], newFilename];
+    NSURL* newUrl = [NSURL URLWithString:newPath];
+    
+    // Check if file exists
+    if (!overwrite && [newUrl checkResourceIsReachableAndReturnError:nil]) {
+        // Exists already
+        [warnings addObject:[NSString stringWithFormat:@"%@ : Skipped (exists)", [[newUrl absoluteString] lastPathComponent]]];
+        return NO;
+    }
+    
+    NSImage *sourceImage = [[NSImage alloc] initWithContentsOfURL:self];
+    if (sourceImage && [sourceImage isValid]) {
+        // Hack to ensure the size is set correctly independent of the dpi
+        NSImageRep *rep = [[sourceImage representations] objectAtIndex:0];
+        [sourceImage setScalesWhenResized:YES];
+        [sourceImage setSize:NSMakeSize([rep pixelsWide], [rep pixelsHigh])];
         
-        // Check if file exists
-		if (!overwrite && [newUrl checkResourceIsReachableAndReturnError:nil]) {
-			// Exists already
-			[warnings addObject:[NSString stringWithFormat:@"%@ : Skipped (exists)", [[newUrl absoluteString] lastPathComponent]]];
-			return NO;
-		}
+        // Warn if either dimension is odd
+        if (((((int)[sourceImage size].width) % 2 != 0) && (((int)[sourceImage size].width) != 1)) ||
+            ((((int)[sourceImage size].height) % 2 != 0) && (((int)[sourceImage size].height) != 1)))
+        {
+            [warnings addObject:[NSString stringWithFormat:@"%@ : has dimensions not divisible by 2", [[self absoluteString] lastPathComponent]]];
+        }
         
-		NSImage *sourceImage = [[NSImage alloc] initWithContentsOfURL:self];
-        if (sourceImage && [sourceImage isValid]) {
-            // Hack to ensure the size is set correctly independent of the dpi
-            NSImageRep *rep = [[sourceImage representations] objectAtIndex:0]; 
-			[sourceImage setScalesWhenResized:YES]; 
-			[sourceImage setSize:NSMakeSize([rep pixelsWide], [rep pixelsHigh])]; 
+        // Determine the image type
+        NSBitmapImageFileType imageType = [self imageType];
+        if ((int)imageType >= 0) {
             
-            // Warn if either dimension is odd
-			if (((((int)[sourceImage size].width) % 2 != 0) && (((int)[sourceImage size].width) != 1)) ||
-				((((int)[sourceImage size].height) % 2 != 0) && (((int)[sourceImage size].height) != 1)))
-			{
-				[warnings addObject:[NSString stringWithFormat:@"%@ : has dimensions not divisible by 2", [[self absoluteString] lastPathComponent]]];
-			}
+            CGFloat _newWidth = ((((int)[sourceImage size].width) == 1) ? 1 : ([sourceImage size].width / 2.0));
+            CGFloat _newHeight = ((((int)[sourceImage size].height) == 1) ? 1 : ([sourceImage size].height / 2.0));
             
-            // Determine the image type
-            NSBitmapImageFileType imageType = [self imageType];
-            if ((int)imageType >= 0) {
-                
-				CGFloat _newWidth = ((((int)[sourceImage size].width) == 1) ? 1 : ([sourceImage size].width / 2.0));
-				CGFloat _newHeight = ((((int)[sourceImage size].height) == 1) ? 1 : ([sourceImage size].height / 2.0));
-				
-				// Create a bitmap representation
-                NSBitmapImageRep *imageRep = [NSBitmapImageRep imageRepWithWidth:_newWidth andHeight:_newHeight];
-                [imageRep setImage:sourceImage];
-
-                // Write out the new image
-                NSData *imageData = [imageRep representationUsingType:imageType properties:nil];
-                if (![imageData writeToURL:newUrl atomically:YES]) {
-                    [errors addObject:[NSString stringWithFormat:@"%@ : Error creating file", newPath]];
-                } 
-                else {
-                    success = YES;
-                }
+            // Create a bitmap representation
+            NSBitmapImageRep *imageRep = [NSBitmapImageRep imageRepWithWidth:_newWidth andHeight:_newHeight];
+            [imageRep setImage:sourceImage];
+            
+            // Write out the new image
+            NSData *imageData = [imageRep representationUsingType:imageType properties:nil];
+            if (![imageData writeToURL:newUrl atomically:YES]) {
+                [errors addObject:[NSString stringWithFormat:@"%@ : Error creating file", newPath]];
             }
             else {
-                [errors addObject:[NSString stringWithFormat:@"%@ : Unknown image type", [[self absoluteString] lastPathComponent]]];
+                success = YES;
             }
         }
         else {
-            // Invalid
+            [errors addObject:[NSString stringWithFormat:@"%@ : Unknown image type", [[self absoluteString] lastPathComponent]]];
+        }
+    }
+    else {
+        // Invalid
         //    [errors addObject:[NSString stringWithFormat:@"%@ : Appears to be invalid", [[self absoluteString] lastPathComponent]]];
-        }
-        
-        // Cleanup
-        if (sourceImage) {
-            [sourceImage release];
-        }
-   // }
-    //else if (errors) {
-        // Not a valid retina file
-        
-    //    [errors addObject:[NSString stringWithFormat:@"%@ : Not a @2x or -hd file", [[self absoluteString] lastPathComponent]]];
-   // }
+    }
     
+    // Cleanup
+    if (sourceImage) {
+        [sourceImage release];
+    }
+    // }
+    //else if (errors) {
+    // Not a valid retina file
+    
+    //    [errors addObject:[NSString stringWithFormat:@"%@ : Not a @2x or -hd file", [[self absoluteString] lastPathComponent]]];
+    // }
+    if([newUrl isRetinaImage]){
+        [newUrl unretina:folder errors:errors warnings:warnings overwrite:overwrite];
+    }
     return success;
 }
 
